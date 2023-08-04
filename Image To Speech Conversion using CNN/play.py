f88742437fa4c291dad4672a4f762b5156061ac3
Priya Chougale <89589377+priya280@users.noreply.github.com>
2023-08-04 19:22:02 +0530

    Add files via upload

---
+++ Image To Speech Conversion using CNN/play.py
@@ -0,0 +1,77 @@
+from keras.models import load_model
+import cv2
+import numpy as np
+from random import choice
+
+REV_CLASS_MAP = {
+    0: "rock",
+    1: "paper",
+    2: "scissors",
+    3: "none"
+}
+
+
+def mapper(val):
+    return REV_CLASS_MAP[val]
+
+
+def calculate_winner(move1, move2):
+    if move1 == move2:
+        return "Tie"
+
+    if move1 == "rock":
+        if move2 == "scissors":
+            return "User"
+        if move2 == "paper":
+            return "Computer"
+
+    if move1 == "paper":
+        if move2 == "rock":
+            return "User"
+        if move2 == "scissors":
+            return "Computer"
+
+    if move1 == "scissors":
+        if move2 == "paper":
+            return "User"
+        if move2 == "rock":
+            return "Computer"
+
+
+model = load_model("rock-paper-scissors-model.h5")
+
+cap = cv2.VideoCapture(0)
+
+prev_move = None
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    # rectangle for user to play
+    cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
+    # rectangle for computer to play
+    cv2.rectangle(frame, (800, 100), (1200, 500), (255, 255, 255), 2)
+
+    # extract the region of image within the user rectangle
+    roi = frame[100:500, 100:500]
+    img = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (300, 300))
+
+    # predict the move made
+    pred = model.predict(np.array([img]))
+    move_code = np.argmax(pred[0])
+    user_move_name = mapper(move_code)
+
+    print(user_move_name)
+
+
+    cv2.imshow("Rock Paper Scissors", frame)
+
+    k = cv2.waitKey(10)
+    if k == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
